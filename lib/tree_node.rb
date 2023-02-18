ef1e4048b529e3875f8829359cd23c21d8931772
chrisbanas <bananas595@gmail.com>
2023-02-17 16:24:43 -0800

    complete

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,6 +1,8 @@
+require "byebug"
+
 class PolyTreeNode
 
-    attr_reader :value, :parent, :children
+    attr_reader :value, :parent, :children, :dfs
 
     def initialize(value, children = [])
         @value = value
@@ -31,6 +33,36 @@ class PolyTreeNode
         else
             child.parent = nil
         end
+    end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+
+        self.children.each do |child|
+            search = child.dfs(target_value)
+            return search unless search.nil?
+        end
+
+        nil
+
+    end
+
+
+    def bfs(target_value)
+        # return self if self.value == target_value
+
+        queue = [self]
+
+        until queue.empty?
+            # debugger
+            node = queue.shift
+            return node if node.value == target_value
+            queue.concat(node.children) unless node.nil?
+        end
+
+        nil
+
+
     end
 
 end
