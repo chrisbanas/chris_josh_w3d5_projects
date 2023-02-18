438c1178b6d0d08945993fd5d156c5a35f55cd58
Josh van Eyken <josh.vaneyken@gmail.com>
2023-02-17 18:28:51 -0800

    start valid_moves method

--- knight's_travails/knight_path_finder.rb
+++ knight's_travails/knight_path_finder.rb
@@ -2,14 +2,36 @@ require_relative "poly_tree_node.rb"
 
 class KnightPathFinder
 
+    def self.valid_moves(pos)
+        # put every move that's valid from position
+        #return array
 
+        # [-2, -1]
+        # [-2, 1]
+        # [-1, -2]
+    end
 
-    attr_reader :root_node
+    attr_reader :root_node, :considered_positions
 
     def initialize(start_pos)
         @start_pos = start_pos
         @root_node = PolyTreeNode.new(start_pos)
+        self.build_move_tree
+        @considered_positions = [start_pos]
     end
+
+    def build_move_tree
+        root_node
+    end
+
+    def new_move_positions(pos)
+        @considered_positions
+        KnightPathFinder.valid_moves
+    end
+
+
+
+
 
 
 
@@ -23,6 +45,7 @@ class KnightPathFinder
 end
 
 if $PROGRAM_NAME == __FILE__
-     a = KnightPathFinder.new([0,0])
+     p a = KnightPathFinder.new([0,0])
      p a.root_node.value
+     p a.considered_positions
 end
